ec1ddc522483ba66a0ce9c26ecf6e2f653f8f447
Henning Thomann <102948323+default-myfault@users.noreply.github.com>
2024-04-11 22:43:40 +0200

    Update btw.lua

--- lua/custom/plugins/btw.lua
+++ lua/custom/plugins/btw.lua
@@ -3,7 +3,11 @@ return {
   config = function()
     require('btw').setup {
       text = [[
-      ]],
+           へ         ╱|、
+      ૮  -   ՛ ) つ(>   < 7  
+       /   ⁻  ៸          、˜〵     
+乀 (ˍ,  ل            じしˍ,)ノ 
+    ]],
     }
   end,
 }
